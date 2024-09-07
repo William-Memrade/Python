e7feaf0820c3e44a3785f88e40caad69fd760a8e
William-Memrade <guillermo.hernandez.andrade@gmail.com>
2024-09-07 14:54:29 -0600

    Updated

--- Session1/factorial.py
+++ Session1/factorial.py
@@ -1,11 +1,24 @@
 # Programa para calcular factoriales
 # 5! = 5 * 4 * 3 * 2 * 1
 
+# for
+
 n = int(input('Digite un numero: '))
 
 f = 1
 
-for n in range(2, n+1):
-    f *= n
+for i in range(2, n + 1):
+    f *= i
+
+print(f'El factorial de {n} es: {f}')
+
+# while
+
+f = 1
+i = 2
+
+while i < n + 1:
+    f *= i
+    i += 1
 
 print(f'El factorial de {n} es: {f}')
